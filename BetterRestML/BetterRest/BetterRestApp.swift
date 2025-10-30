b12a6ba215bb34cd4cb71b20e1ec5c7e5519f987
korayurun <korayurun07@gmail.com>
2025-10-30 16:55:24 +0300

    BetterRest app which calculate the time you should sleep is added

---
+++ BetterRestML/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Koray Urun on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
