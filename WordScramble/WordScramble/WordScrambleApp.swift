f0ba021d391716fd12fdec90c4185a5ae7c168ba
korayurun <korayurun07@gmail.com>
2025-10-31 20:46:54 +0300

    WordScramble app is added

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Koray Urun on 31.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
