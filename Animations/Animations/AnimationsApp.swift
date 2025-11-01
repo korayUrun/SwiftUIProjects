ae2ed2e4b69c7c594189e890c8b6b30f651d8a74
korayurun <korayurun07@gmail.com>
2025-11-01 11:53:10 +0300

    Animations Project is applied and I've experienced some animation concepst

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Koray Urun on 1.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
