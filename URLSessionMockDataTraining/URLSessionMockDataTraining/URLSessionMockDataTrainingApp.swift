cafb95df61285d87ce3debe8176d0f46e245f377
korayurun <korayurun07@gmail.com>
2026-02-04 03:01:49 +0300

    UserDefaults | @AppStorage | using @Environment to access default user preferrences

---
+++ URLSessionMockDataTraining/URLSessionMockDataTraining/URLSessionMockDataTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionMockDataTrainingApp.swift
+//  URLSessionMockDataTraining
+//
+//  Created by Koray Urun on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionMockDataTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
