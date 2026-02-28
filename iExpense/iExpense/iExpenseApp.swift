e623d467a5b66f8338519476943b204b319de728
korayurun <korayurun07@gmail.com>
2026-02-28 13:06:50 +0300

    iExpense basic expense saving app

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Koray Urun on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
